0ae92ede29d7f246e9e82a76a2468adcc45dea87
Tadayoshi123 <yankill@outlook.fr>
2023-01-18 17:12:44 +0100

    Feature : User_controller : Added and checked CRUD for current user and added special CRUD requests for Admin

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,7 +12,49 @@ exports.like = async (req, res, next) => {
   res.send("successfully logged in");
 }; 
 
+
 exports.getUser = async (req, res, next) => {
+  User.findById(req.userToken.id)
+  .then(user => {
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found",
+      });
+    }
+    res.send(user);
+  })
+  .catch(error => res.status(400).send(error));
+};
+
+
+exports.updateUser = async (req, res, next) => {
+  User.findByIdAndUpdate(req.userToken.id, req.body)
+  .then((user) => {
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found",
+      });
+    }
+    User.findById(user._id).then(userupdated => {
+      res.send(userupdated);
+    })
+  })
+  .catch(error => res.status(400).send(error));
+};
+
+exports.deleteUser = async (req, res, next) => {
+  User.findByIdAndDelete(req.userToken.id).then(user => {
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found",
+      });
+    }
+    res.send(user);
+  })
+  .catch(error => res.status(400).send(error));
+};
+
+exports.getUserAdmin = async (req, res, next) => {
   User.findById(req.params.id)
   .then(user => {
     if (!user) {
@@ -25,7 +67,7 @@ exports.getUser = async (req, res, next) => {
   .catch(error => res.status(400).send(error));
 };
 
-exports.updateUser = async (req, res, next) => {
+exports.updateUserAdmin = async (req, res, next) => {
   User.findByIdAndUpdate(req.params.id, req.body)
   .then((user) => {
     if (!user) {
@@ -40,8 +82,13 @@ exports.updateUser = async (req, res, next) => {
   .catch(error => res.status(400).send(error));
 };
 
-exports.deleteUser = async (req, res, next) => {
+exports.deleteUserAdmin = async (req, res, next) => {
   User.findByIdAndDelete(req.params.id).then(user => {
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found with id " + req.params.id,
+      });
+    }
     res.send(user);
   })
   .catch(error => res.status(400).send(error));
--- routes/user.route.js
+++ routes/user.route.js
@@ -9,10 +9,15 @@ const {verifyAdmin} = require('../middlewares/verifyAdmin');
 const verifyToken = require('../middlewares/verifyToken');
 
 
-router.get('/user/:id', verifyToken, userController.getUser);
-router.put('/user/:id',  userController.updateUser);
-router.delete('/user/:id',  userController.deleteUser);
+router.get('/user', verifyToken, userController.getUser);
+router.put('/user',  verifyToken, userController.updateUser);
+router.delete('/user',  verifyToken, userController.deleteUser);
 router.get('/users',  userController.getAllUsers);
+
+// TODO: Avoir un utilisateur admin pour utiliser les routes ci-dessous :
+// router.get('/useradmin/:id', verifyAdmin, userController.getUserAdmin);
+// router.put('/useradmin/:id', verifyAdmin, userController.updateUserAdmin);
+// router.delete('/useradmin/:id', verifyAdmin, userController.deleteUserAdmin);
 
 module.exports = router;
 
