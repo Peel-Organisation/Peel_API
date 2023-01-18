65f9b86d103b40cea5554ea926d22b7554c595c1
Tadayoshi123 <yankill@outlook.fr>
2023-01-18 11:20:59 +0100

    feat(controller): add request to auth route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,61 @@
+const User = require("/models/user.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 exports.register = async (req, res, next) => {
-  res.send("successfully registered");
+  
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+});
+
+  try {
+    const newUserToSave = await newUser.save();
+    return res.send(newUserToSave);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.login = async (req, res, next) => {
-  res.send("successfully logged in");
+  User.findOne({ email: req.body.email })
+  .then((user) => {
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        message: "Password Is Not valid",
+        auth: false,
+      })
+    }
+    let userToken = jwt.sign({
+      id: user._id,
+
+    }, )
+    res.send({
+      message: "User successfully logged in",
+      auth: true,
+      token: userToken,
+    })
+  })
+  .catch(error => res.Status(400).send(error))
 };
 
 exports.getUserByToken = async (req, res, next) => {
-  res.send("successfully logged");
+  const token = req.headers.authorization.split(" ")[1];
+  console.log(token);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  User.findById(decoded.id)
+  .then((user) => {
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+    res.send(user);
+  })
+  .catch(error => res.Status(400).send(error))
 };
