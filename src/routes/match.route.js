12c52494fe99c671f9c799cef682fb5372a5b70a
KevOneRedOne <kevinalves@hotmail.fr>
2023-03-08 16:26:44 +0100

    feat(match): Update the endpoint

--- src/routes/match.route.js
+++ src/routes/match.route.js
@@ -6,11 +6,9 @@ const matchController = require('../controllers/match.controller');
 const verifyToken = require('../middlewares/verifyToken');
 
 router.get('/swipeProfil', verifyToken, matchController.getSwipeProfil);
-router.get('/swipeVocal', verifyToken, matchController.getSwipeVocal);
 router.post('/like_dislike/:id', verifyToken, matchController.PutLikeDislike);
 
 module.exports = router;
 
 // 	-getSwipeProfilUser 
-// 	-getSwipeVocalUser 
-// 	-likeUser
+
