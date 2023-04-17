5b6bbfaac6df602ca12c558409474639accbdcd8
Damien Drozd <damien.drozd@icloud.com>
2023-04-17 11:18:03 +0200

    edit pull request

--- faker-update.js
+++ faker-update.js
@@ -2,7 +2,7 @@ const { faker } = require('@faker-js/faker');
 faker.setLocale('fr')
 const axios = require('axios');
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzY2Q5NDRhNzMwYjQ4OWExZWZjMDBkYSIsImlhdCI6MTY3NDQ4NDgwNn0.yUTDumUU346yPxMGwED6GILD3dyuxuEE0_yA7-JpOnM"
+const token = process.env.GIPHY_API_KEY
 
 const getUserList = async () => {
     try {
@@ -10,7 +10,7 @@ const getUserList = async () => {
             headers: { 'Content-Type': 'application/json', "authorization": token },
             method: 'GET'
         };
-        let link = 'http://localhost:3001/api/user/all';
+        let link = `${process.env.API_LINK}/user/all`;
         const response = await fetch(link, requestOptions)
         const dataJson = await response.json();
         let status_code = response.status;
@@ -30,7 +30,7 @@ const getRandomGif = async () => {
             headers: { 'Content-Type': 'application/json' },
             method: 'GET'
         };
-        let link = 'https://api.giphy.com/v1/gifs/random?api_key=dWUao2LMG0bqBu1qHB1g2Dn1MS6Prwev';
+        let link = `${process.env.GIPHY_PATH}/random?api_key=${process.env.GIPHY_API_KEY}`;
         const response = await fetch(link, requestOptions)
         const dataJson = await response.json();
         let status_code = response.status;
@@ -51,7 +51,7 @@ const updateUser = async (user) => {
             method: 'PUT',
             body: JSON.stringify(user)
         };
-        let link = `http://localhost:3001/api/user/useradmin/${user._id}`;
+        let link = `${process.env.API_LINK}/user/useradmin/${user._id}`;
         const response = await fetch(link, requestOptions)
         const dataJson = await response.json();
         let status_code = response.status;
@@ -66,12 +66,10 @@ const updateUser = async (user) => {
 }
 
 getUserList().then((userList) => {
-    // console.log("userList : ", userList)
     if (userList?.length > 0) {
         for (let user of userList) {
             if (user.gif === undefined) {
                 getRandomGif().then((gif) => {
-                    // console.log("gif : ", gif)
                     user.gif = {
                         id: gif.id,
                         url: gif.url,
@@ -85,7 +83,6 @@ getUserList().then((userList) => {
                             "hash": gif?.images?.original?.hash
                         }
                     }
-                    console.log("user : ", user.gif)
                     updateUser(user).then((updatedUser) => {
                         console.log("updatedUser : ", updatedUser)
                     })
