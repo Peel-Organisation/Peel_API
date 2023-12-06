a0168789de166969adc45d0d6ee68b824835712c
KevOneRedOne <kevinalves@hotmail.fr>
2023-12-06 16:09:18 +0100

    fix(Log): trust proxy

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   if (process.env.STACK === 'development') {
     const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
-    const clientIp = (req.headers['x-forwarded-for'] || '').split(',').shift().replace('::ffff:', '') || req.ip;
+    const clientIp = (req.headers['x-forwarded-for'] || '').split(',').shift().replace('::ffff:', '') || req.socket.remoteAddress;
     console.log(`[${currentTime}] - Requête reçue : ${req.method}, ${req.url}, ${JSON.stringify(req.params)}, ${JSON.stringify(req.body)} | l'adresse IP source : [${clientIp}] `);
     if (req.method === 'POST' || req.method === 'PUT') {
       console.log(`[${currentTime}] - Corps de la requête :  ${JSON.stringify(req.body)} | de l'adresse IP:  [${clientIp}] `);
