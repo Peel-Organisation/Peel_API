949e1fb9c925fb9d73bbe13e1b20633b0658a008
Kévin Alves <78157227+KevOneRedOne@users.noreply.github.com>
2023-12-10 12:45:50 +0100

    Add security and ip tracability (#44)
    * fix(Log): add date on api log
    
    * fix(Log): stringify data
    
    * fix(Log): stringify data
    
    * fix(Log): add security and ip client print
    
    * fix(Log): ipv4 address
    
    * fix(Log): trust proxy

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const apiRouter = require('./routes');
+const moment = require('moment');
+const helmet = require('helmet');
 
 
 const bodyParser = require('body-parser')
 const app = express()
 app.use(cors());
+app.use(helmet());
 
+// Trust the headers set by your reverse proxy
+app.set('trust proxy', true);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -18,10 +23,9 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   if (process.env.STACK === 'development') {
-    const currentTime = new Date().toISOString();
-    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log(`[${currentTime}] - Requête reçue : ${req.method}, ${req.url}, ${JSON.stringify(req.params)}, ${JSON.stringify(req.body)} | de l'adresse IP:  [${clientIp}] `);
-
+    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    const clientIp = (req.headers['x-forwarded-for'] || '').split(',').shift().replace('::ffff:', '') || req.socket.remoteAddress;
+    console.log(`[${currentTime}] - Requête reçue : ${req.method}, ${req.url}, ${JSON.stringify(req.params)}, ${JSON.stringify(req.body)} | l'adresse IP source : [${clientIp}] `);
     if (req.method === 'POST' || req.method === 'PUT') {
       console.log(`[${currentTime}] - Corps de la requête :  ${JSON.stringify(req.body)} | de l'adresse IP:  [${clientIp}] `);
     }
@@ -33,6 +37,9 @@ app.use((req, res, next) => {
 });
 
 app.use('/api', apiRouter);
+
+// Reduce Fingerprinting
+app.disable('x-powered-by')
 
 
 module.exports = app;
