4fa1859a3e0017efb41a3daee797bbd50a84427f
Damien Drozd <damien.drozd@icloud.com>
2023-12-22 16:27:15 +0100

    fix(backend): firebase massaging token facultative

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,16 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
-const {
-  checkEmail, checkIdentity, checkPassword, validation,
-} = require('../middlewares/validators');
 const verifyToken = require('../middlewares/verifyToken');
 const saveToken = require('../middlewares/saveToken');
+const verifyProfileCompleted = require('../middlewares/verifyProfileCompleted');
 
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get("/protected", verifyToken, saveToken, authController.getUserByToken);
+router.get('/verifyProfileCompleted', verifyToken, verifyProfileCompleted, authController.getUserByToken);
 
 
 
